c96f442ae8d53e4b18cd6203a49ae3c387803b4f
PDRdo7 <140813686+PDRdo7@users.noreply.github.com>
2023-11-30 13:36:39 -0300

    Add files via upload

---
+++ metal-store/loja.js
@@ -0,0 +1,13 @@
+ //adicionar ao carrinho
+ function Adicione(){
+    console.log("teste");
+    document.querySelector(".novoItem").textContent = document.querySelector(".produto").textContent;
+}
+//remover do carrino
+function Remove(){
+    document.querySelector(".produto-carrinho").textContent = " ";
+    }
+    function Remove2(){
+    document.querySelector(".novoItem").textContent = " ";
+    }
+    
